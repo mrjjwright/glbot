9d1c7082728422b6a6763bd43ef52a8d3e0e639c
JJ Wright <mrjjwright@gmail.com>
2024-12-14 17:25:30 -0700

    Enhance TextArea component with autoPlay functionality, dynamic resizing, and improved line counting. Update server middleware for CORS handling and viewer session management. Add TypeScript declarations for additional file types and update CSS for font styling.

--- types/files.d.ts
+++ types/files.d.ts
@@ -2,18 +2,3 @@ declare module '*.txt' {
   const content: string
   export default content
 }
-
-declare module '*.md' {
-  const content: string
-  export default content
-}
-
-declare module '*.xml' {
-  const content: string
-  export default content
-}
-
-declare module '*.module.scss' {
-  const classes: { [key: string]: string }
-  export default classes
-}
